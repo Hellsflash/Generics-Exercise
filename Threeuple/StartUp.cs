827b2ed2d22344339ab06f7ea6e41e84e5c1fc92
Mario Slavov <hellsflash@gmail.com>
2017-07-23 18:10:57 +0300

    Solved 11-12 from Gemerics Exercise

---
+++ Threeuple/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+
+        var input = Console.ReadLine().Split();
+        var names = $"{input[0]} {input[1]}";
+        var address = input[2];
+        var city = input[3];
+        var firstThree = new Threeuple<string, string, string>(names, address, city);
+
+        var secondInput = Console.ReadLine().Split();
+        var name = secondInput[0];
+        var litersOfBeer = int.Parse(secondInput[1]);
+        var drunkOrNot = secondInput[2];
+        if (drunkOrNot == "drunk")
+        {
+            drunkOrNot = "True";
+        }
+        else
+        {
+            drunkOrNot = "False";
+        }
+        var secondThree = new Threeuple<string, int, string>(name, litersOfBeer, drunkOrNot);
+
+        var thirdInput = Console.ReadLine().Split();
+        var nameof = thirdInput[0];
+        var accBalance = double.Parse(thirdInput[1]);
+        var bankName = thirdInput[2];
+        var thirdThree = new Threeuple<string, double, string>(nameof, accBalance, bankName);
+
+        Console.WriteLine(firstThree);
+        Console.WriteLine(secondThree);
+        Console.WriteLine(thirdThree);
+    }
+}
---
+++ Tuple/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var firstPair = Console.ReadLine().Split();
+        var names = $"{firstPair[0]} {firstPair[1]}";
+        var addres = firstPair[2];
+        var firstTuple = new Tuple<string, string>(names, addres);
+
+        var secondPair = Console.ReadLine().Split();
+        var name = secondPair[0];
+        var beerAmount = int.Parse(secondPair[1]);
+        var secondTuple = new Tuple<string, int>(name, beerAmount);
+
+        var thirdPair = Console.ReadLine().Split();
+        var intItem = int.Parse(thirdPair[0]);
+        var doubleItem = double.Parse(thirdPair[1]);
+        var thirdTuple = new Tuple<int, double>(intItem, doubleItem);
+
+        Console.WriteLine(firstTuple);
+        Console.WriteLine(secondTuple);
+        Console.WriteLine(thirdTuple);
+    }
+}
